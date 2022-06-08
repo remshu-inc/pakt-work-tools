ce2994c450250f6ba32b5031f70870d294cb31e3
remshu <39970371+remshu@users.noreply.github.com>
2022-06-08 05:58:07 +0300

    Update views.py

--- search_app/views.py
+++ search_app/views.py
@@ -4,87 +4,122 @@ from django.db.models import Q
 import re
 
 def index(request):
-    
     return render(request, "index.html")
 
+
+def _filter_shaping(cql):
+    """Формирование филтра на основе cql
+
+    Keyword arguments:
+    cql -- Запроса пользователя
+
+    """
+    
+    # Получение аттрибута запроса
+    word = re.search(r'[\'\"].*[\'\"]', cql).group(0)[1:-1]
+    
+    # Удаление всех пробелов
+    cql = cql.replace(" ", "")
+    
+    # Обработка словоформ
+    if 'word=' in cql:
+        return Q(token_id__text__iexact = word)
+        
+    # Обрабокта тегов ошибок и частеречной разметки
+    elif 'error=' in cql or 'pos=' in cql:
+        return Q(Q(tag_id__tag_text = word) | Q(tag_id__tag_text_russian = word))
+        
+    # Обработка словоформ
+    if 'word!=' in cql:
+        return ~Q(token_id__text__iexact = word)
+        
+    # Обрабокта тегов ошибок и частеречной разметки
+    elif 'error!=' in cql or 'pos=' in cql:
+        return ~Q(Q(tag_id__tag_text = word) | Q(tag_id__tag_text_russian = word))
+    
+    return None
+        
+
+def _parse_cql(user_query = None):
+    """Парсинг Corpus Query Language из запроса пользователя
+
+    Keyword arguments:
+    user_query -- Запрос пользователя (default None)
+
+    """
+    
+    # Получение cql из запроса пользователя
+    cql = re.findall(r'\[[^\[\]]+\]', user_query)
+    
+    # Формирование фильтра для поиска в БД
+    filters = Q()
+    
+    for token_cql in cql:
+        
+        # Парсинг нескольких параметров
+        if "&" in token_cql:
+            parts_token_cql = token_cql[1:-1].split('&')
+            for part in parts_token_cql:
+                
+                filters &= _filter_shaping(part)
+            print(filters)
+        
+        # Парсинг альтернативных вариантов
+        # TODO: Исправить вывод дублированных текстов(где совпадает и токен, и тег)
+        elif "|" in token_cql:
+            parts_token_cql = token_cql[1:-1].split('|')
+            alt_filters = Q()
+            for part in parts_token_cql:
+                
+                alt_filters |= _filter_shaping(part)
+
+            print(alt_filters)
+            filters &= alt_filters
+                
+        
+        else:
+            filters &= _filter_shaping(token_cql)
+    
+    return filters
+                    
+                    
 def search(request):
     if request.POST:
-        corpus_search = request.POST['corpus_search']
-        corpus_search = re.findall(r'\[[^\[\]]+\]', corpus_search)
+        user_query = request.POST['corpus_search']
+        filters = _parse_cql(user_query)
+        
+        # Получение строк по заданным условиям
+        sentence_objects = TblMarkup.objects.filter(filters).values(
+            'token_id', 'token_id__sentence_id', 'token_id__sentence_id__text_id__header', 'token_id__sentence_id__text_id__create_date'
+        )[:100]
+        
+        # TODO: пропписать исключение
+        if len(sentence_objects) == 0:
+            pass
+        
+        list_search = []
+        for sentence in sentence_objects:
+            tokens = TblToken.objects.filter(
+                sentence_id = sentence['token_id__sentence_id']
+            ).order_by('order_number')
+            
+            list_token = []
+            for token in tokens:
+                if token.text == '-EMPTY-':
+                    continue
+                if token.id_token == sentence['token_id']:
+                    list_token.append({'text': token.text, 'primary': True})
+                else:
+                    list_token.append({'text': token.text})
+            
+            list_search.append({
+                'header': sentence['token_id__sentence_id__text_id__header'],
+                'tokens': list_token,
+                'create_date': sentence['token_id__sentence_id__text_id__create_date']
+            })
         
         # Для неточного поиска
         # MyClass.objects.filter(name__iexact=my_parameter)
-        
-        # TODO: написать фор для обработки всех запросов
-        # Поиск только по первому запросу
-        if 'word=' in corpus_search[0] or 'word =' in corpus_search[0]:
-            word = re.search(r'[\'\"].*[\'\"]', corpus_search[0]).group(0)[1:-1]
-            
-            sentence_objects = TblToken.objects.filter(
-                text__iexact = word
-            ).values(
-                'id_token', 'sentence_id', 'sentence_id__text_id__header', 'sentence_id__text_id__create_date'
-            )
-
-            # TODO: пропписать исключение
-            if len(sentence_objects) == 0:
-                pass
-            
-            list_search = []
-            for sentence in sentence_objects:
-                tokens = TblToken.objects.filter(
-                    sentence_id = sentence['sentence_id']
-                ).order_by('order_number')
-                
-                list_token = []
-                for token in tokens:
-                    if token.text == '-EMPTY-':
-                        continue
-                    if token.id_token == sentence['id_token']:
-                        list_token.append({'text': token.text, 'primary': True})
-                    else:
-                        list_token.append({'text': token.text})
-                
-                list_search.append({
-                    'header': sentence['sentence_id__text_id__header'],
-                    'tokens': list_token,
-                    'create_date': sentence['sentence_id__text_id__create_date']
-                })
-        
-        elif 'pos=' in corpus_search[0] or 'pos =' in corpus_search[0]:
-            pass
-        
-        elif 'error=' in corpus_search[0] or 'error =' in corpus_search[0]:
-            error = re.search(r'[\'\"].*[\'\"]', corpus_search[0]).group(0)[1:-1]
-            
-            sentence_objects = TblMarkup.objects.filter(
-                Q(tag_id__tag_text = error) | Q(tag_id__tag_text_russian = error)
-            ).values(
-                'token_id', 'token_id__sentence_id', 'token_id__sentence_id__text_id__header', 'token_id__sentence_id__text_id__create_date'
-            )
-            
-            # TODO: пропписать исключение
-            if len(sentence_objects) == 0:
-                pass
-            
-            list_search = []
-            for sentence in sentence_objects:
-                tokens = TblToken.objects.filter(
-                    sentence_id = sentence['token_id__sentence_id']
-                ).order_by('order_number')
-                
-                list_token = []
-                for token in tokens:
-                    if token.id_token == sentence['token_id']:
-                        list_token.append({'text': token.text, 'primary': True})
-                    else:
-                        list_token.append({'text': token.text})
-                
-                list_search.append({
-                    'header': sentence['token_id__sentence_id__text_id__header'],
-                    'tokens': list_token,
-                    'create_date': sentence['token_id__sentence_id__text_id__create_date']
-                })
 
         return render(request, "search.html", context={'search_value': request.POST['corpus_search'],'list_search': list_search})
 
