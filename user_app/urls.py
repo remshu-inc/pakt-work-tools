c2a9eedff766e2d4fe4b6be659cfc23743eaeffd
remshu <39970371+remshu@users.noreply.github.com>
2022-09-30 22:03:58 +0300

    Register Teacher

--- user_app/urls.py
+++ user_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('logout/', views.log_out, name='logout'),
     path('manage/', views.manage, name='manage'),
     path('manage/signup/', views.signup, name='signup'),
+    path('manage/signup_teacher/', views.signup_teacher, name='signup_teacher'),
     path('manage/group_creation/', views.group_creation, name='group_creation'),
     path('manage/group_modify/', views.group_selection, name = 'group_selection'),
     path('manage/group_modify/<int:group_id>/', views.group_modify, name = 'group_modify'),
