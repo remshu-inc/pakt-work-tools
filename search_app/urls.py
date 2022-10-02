e5494ee8f5dcdcfa6e868aa7262ba827ab37407d
remshu <39970371+remshu@users.noreply.github.com>
2022-10-02 18:13:20 +0300

    FAQ
    - FAQ по CQL
    - Выведен общий результат поиска

--- search_app/urls.py
+++ search_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('cql_faq', views.cql_faq, name='cql_faq'),
     path('search', views.search, name='search'),
     path('search/statistic', views.get_stat, name='statistic')
 ]
