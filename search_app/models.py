04537c5e7131b6b1a26dfd9fd3f725b969652f4f
Kirill Kulakov <kulakov@cs.petrsu.ru>
2023-03-10 15:54:06 +0300

    fix stats creation

--- search_app/models.py
+++ search_app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from user_app.models import TblLanguage
 
-
 from datetime import datetime
+
 
 # Create your models here.
 class TblSystemMetric(models.Model):
@@ -10,9 +10,7 @@ class TblSystemMetric(models.Model):
         db_table = 'TblSystemMetric'
 
     id_metric = models.AutoField(primary_key=True)
-    language = models.ForeignKey(TblLanguage, null = True, on_delete=models.PROTECT)
+    language = models.ForeignKey(TblLanguage, null=True, on_delete=models.PROTECT)
     metric_name = models.CharField(max_length=255)
     metric_value = models.FloatField(default=0)
-    metric_update_time = models.DateTimeField(default=datetime(1970,1,1), null=False)
-
-
+    metric_update_time = models.DateTimeField(default=datetime(1970, 1, 1), null=False)
