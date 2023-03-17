3eab488d2124057ab5c5faa6651d0bb11e7cb056
remshu <39970371+remshu@users.noreply.github.com>
2023-03-17 16:50:37 +0300

    Справочник тегов для НЕМЕЦКОГО
    Отображение:
     - Причина ошибок
     - Степень грубости ошибки
     - Теги ошибок
     - Теги частей речи

--- search_app/urls.py
+++ search_app/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='home'),
     path('cql_faq', views.cql_faq, name='cql_faq'),
+    path('tag_list', views.tag_list, name='tag_list'),
     path('search', views.search, name='search'),
     path('search/<str:text_id>/', views.text, name='text'),
     path('search/statistic', views.get_stat, name='statistic'),
