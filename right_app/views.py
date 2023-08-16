631cd4dbec9ba597b91845610421f8c09907b0ea
Ilya <kotyrov.1310@gmail.com>
2023-08-16 20:47:03 +0200

    restrict access to annotation of other languages

--- right_app/views.py
+++ right_app/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from .models import TblUserRights, TblText
+from .models import TblUserRights, TblText, TblUser
 
 
 def check_permissions_show_text(user_id, text_id=None):
@@ -40,7 +40,12 @@ def check_permissions_edit_text(user_id, text_id=None):
     return permission.exists()
 
 
-def check_permissions_work_with_annotations(user_id, text_id=None):
+def check_permissions_work_with_annotations(user_id, text_id):
+    user_language = TblUser.objects.filter(id_user=user_id).first().language_id
+    text_language = TblText.objects.filter(id_text=text_id).first().language_id
+    if (text_language != user_language):
+        return False
+    
     permission = TblUserRights.objects.filter(
         Q(text_id=text_id) | Q(text_id=None),
         right_id=5, user_id=user_id,
