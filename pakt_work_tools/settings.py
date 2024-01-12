f8757174c08de7e626a81b21f30e93e10981b3ce
Kirill Kulakov <kulakov@cs.petrsu.ru>
2024-01-12 11:24:46 +0300

    Сохранение локально внешних скриптов

--- pakt_work_tools/settings.py
+++ pakt_work_tools/settings.py
@@ -93,8 +93,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'lingo',
-        'USER': 'root',
-        'PASSWORD': 'Vh7PPkk7HvuzEmK9',
+        'USER': 'lingo',
+        'PASSWORD': 'lingolingo',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'CHARSET': 'utf8mb4',
