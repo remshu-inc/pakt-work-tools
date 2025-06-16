2440ec7a39f7a68b3a8cac6152a4df8ffce1082b
Kirill Kulakov <kulakov@cs.petrsu.ru>
2025-06-16 15:29:57 +0300

    fix test settings

--- pakt_work_tools/settings/test.py
+++ pakt_work_tools/settings/test.py
@@ -6,7 +6,12 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lingo',
+        'USER': 'lingo',
+        'PASSWORD': 'lingolingo',
+        'HOST': 'mysql',
+        'PORT': '3306',
+        'CHARSET': 'utf8mb4',
     }
 }
