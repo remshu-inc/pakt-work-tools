369b699f597915b43da8f5403ca2742001337641
Kirill Kulakov <kulakov@cs.petrsu.ru>
2025-06-16 15:07:16 +0300

    code cleanup

--- create_migrations.py
+++ create_migrations.py
@@ -3,9 +3,9 @@ import os
 APPS = {'search_app','user_app', 'right_app','text_app'}
 
 for app in APPS:
-    os.system(f"python3 manage.py makemigrations {app}")
+    os.system(f"python manage.py makemigrations {app}")
 
 next_step = input('Продолжить? (Y/N)')
 
 if next_step.lower() == 'y':
-    os.system("python3 manage.py migrate")
+    os.system("python manage.py migrate")
