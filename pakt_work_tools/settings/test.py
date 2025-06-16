7ee389632f0f9729a068b2dbaea183b8ec077fa1
Kirill Kulakov <kulakov@cs.petrsu.ru>
2025-06-16 15:31:45 +0300

    fix test settings

--- pakt_work_tools/settings/test.py
+++ pakt_work_tools/settings/test.py
@@ -10,7 +10,7 @@ DATABASES = {
         'NAME': 'lingo',
         'USER': 'lingo',
         'PASSWORD': 'lingolingo',
-        'HOST': 'mysql',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'CHARSET': 'utf8mb4',
     }
